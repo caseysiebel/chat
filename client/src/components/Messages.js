f77de33a431132eecb0b3434b6201f7f2843d22a
Casey Siebel <leptone55@gmail.com>
2017-10-23 17:40:33 -0700

    Add API call

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 import Message from './Message';
 class Messages extends React.Component {
+    componentDidMount(){
+        const request = async () => {
+            const response = await fetch('/messages');
+            const json = await response.json();
+            /*
+            const messages = JSON.parse(json)
+            if (messages.length) {
+                populateMessages(messages);
+            }
+            */
+        };
+        request();
+    }
     render() {
         console.log('props!!!!!', this.props);
         const messages = this.props.messages;
@@ -15,8 +28,13 @@ class Messages extends React.Component {
         );
     }
 }
+const populateMessages = (messages) => ({
+    type: 'POPULATE_MESSAGES',
+    payload: messages
+})
+const actions = { populateMessages };
 
 const mapStateToProps = ({ messages }) => ({ messages });
 import { connect } from 'react-redux';
-const connectedMessages = connect(mapStateToProps)(Messages);
+const connectedMessages = connect(mapStateToProps, actions)(Messages);
 export default connectedMessages;
