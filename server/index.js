133b2d4c87cb35cde632ae2cb3a5ee3ae62cefdf
Casey Siebel <leptone55@gmail.com>
2017-10-23 17:41:03 -0700

    Create /message route

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const http = require('http');
 const express =  require('express');
 const socketIO = require('socket.io');
+const mongojs = require('mongojs');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static(__dirname + '/public'));
+const db = mongojs('chat', ['messages']);
+
+app.get('/messages', (req, res) => {
+    console.log('in message route');
+    db.message.find({}, (err, messages) => res.json(messages))
+});
 
 io.on('connection', (socket) => {
     //console.log('a user connection');
@@ -14,8 +20,11 @@ io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         console.log('chat message recieved', msg);
         io.emit('chat message', msg);
+        db.messages.insert(msg);
     });
 });
+
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
